bc8c4370720465029e5e2100bce2938514747ad7
bryansevnikolay-netizen <bryansev.nikolay@gmail.com>
2026-03-16 21:10:46 +0300

    Update Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,11 @@
 #include <string>
 
 int main() {
-    std::string name;
+  std::string name;
   // only comment no more
-std::cout << "Please enter name";
-    std::cin >> name;
-    std::cout << "Hello world from " << name;
+  std::cout << "Please enter name";
+  std::cin >> name;
+  std::cout << "Hello world from " << name;
+
+  return 0;
 }
